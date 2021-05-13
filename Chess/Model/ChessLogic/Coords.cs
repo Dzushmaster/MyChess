8f6c5d63cb593703a8360d14674bc0fe50dbcc71
Dzushmaster <dimon_228_2@mail.ru>
2021-05-13 17:51:28 +0300

    Add coords

---
+++ Chess/Model/ChessLogic/Coords.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Model.ChessLogic
+{
+    sealed class Coords
+    {
+        public SByte Row { get; set; }
+        public SByte Column { get; set; }
+        public Coords(SByte Row, SByte Column)
+        {
+            this.Row = Row;
+            this.Column = Column;
+        }
+        public override string ToString()
+        {
+            return Row + ";" + Column;
+        }
+    }
+}
