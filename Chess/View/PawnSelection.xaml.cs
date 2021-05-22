76a858ca8b01b1d0ceb47dafe3f4e76b5dd625d3
Dzushmaster <dimon_228_2@mail.ru>
2021-05-22 13:08:15 +0300

    Added window for choosing figure

---
+++ Chess/View/PawnSelection.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chess.View
+{
+    /// <summary>
+    /// Логика взаимодействия для PawnSelection.xaml
+    /// </summary>
+    public partial class PawnSelection : Window
+    {
+        public char Status { get; private set; }
+        public PawnSelection(bool isWhite)
+        {
+            InitializeComponent();
+            Image img;
+            for(int i = 0; i < 4; i++)
+            {
+                img = new Image();
+                img.SetValue(RenderOptions.BitmapScalingModeProperty, BitmapScalingMode.Fant);
+                img.IsHitTestVisible = false;
+                switch(i)
+                {
+                    case 0:
+                        if (isWhite)
+                            img.Source = new BitmapImage(new Uri("/COURSE_PROJECT/Image/Pieces/White_Rook.png"));
+                        else
+                            img.Source = new BitmapImage(new Uri("/COURSE_PROJECT/Image/Pieces/Black_Rook.png"));
+                        break;
+                    case 1:
+                        if (isWhite)
+                            img.Source = new BitmapImage(new Uri("/COURSE_PROJECT/Image/Pieces/White_Bishop.png"));
+                        else
+                            img.Source = new BitmapImage(new Uri("/COURSE_PROJECT/Image/Pieces/Black_Bishop.png"));
+                        break;
+                    case 2:
+                        if (isWhite)
+                            img.Source = new BitmapImage(new Uri("/COURSE_PROJECT/Image/Pieces/White_Knight.png"));
+                        else
+                            img.Source = new BitmapImage(new Uri("/COURSE_PROJECT/Image/Pieces/Black_Knight.png"));
+                        break;
+                    case 3:
+                        if (isWhite)
+                            img.Source = new BitmapImage(new Uri("/COURSE_PROJECT/Image/Pieces/White_Queen.png"));
+                        else
+                            img.Source = new BitmapImage(new Uri("/COURSE_PROJECT/Image/Pieces/Black_Queen.png"));
+                        break;
+                }
+
+            }
+        }
+
+        private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source is Button)
+            {
+                if (e.Source == button_rook)
+                    Status = 'r';
+                else if (e.Source == button_bishop)
+                    Status = 'b';
+                else if (e.Source == button_knight)
+                    Status = 'h';
+                else if (e.Source == button_queen)
+                    Status = 'q';
+                Close();
+            }
+            else
+                DragMove();
+        }
+    }
+}
